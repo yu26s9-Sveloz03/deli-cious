dd245bc561d73e3576b356d2df46b7b1a3af3575
Sveloz03 <velozsam@gmail.com>
2026-05-25 20:06:34 -0400

    started UI

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Main {
+
+    static void main() {
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.homeScreen();
+    }
+}
---
+++ src/main/java/com/pluralsight/Order.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.List;
+
+public class Order {
+    private List<Item> items;
+
+    public Order(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotalPrice() {
+        double totalPrice = 0.0;
+        for (Item item : items) {
+            totalPrice += item.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+}
